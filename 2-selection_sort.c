91f0c0b86b7a7236afae1104fd1326a0a94ba005
CodexRodney <rodney002mwanje@gmail.com>
2022-01-12 02:45:10 +0300

    Fixed betty coding style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,18 +26,18 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
-	if(size < 2)
+	if (size < 2)
 		return;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for(j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if(array[j] < array[min_idx])
 				min_idx = j;
 		}
-		if(min_idx == i)
+		if (min_idx == i)
 			continue;
 		swap(&array[min_idx], &array[i]);
 		print_array(array, size);
